482c795dd8da446235cf03eae6dedd2bc1787e30
Ajay <83897372+ajaybhupathiraju@users.noreply.github.com>
2024-07-16 00:03:22 +0530

    Update sql_queries.sql

--- data/sql_queries.sql
+++ data/sql_queries.sql
@@ -1,7 +1,7 @@
 		/** Below is a list of questions asked by the business team (senior sales executive). **/
 
 
-1.	Provide a list of products with a base price greater than 500 and that are featured in promo type of 'BOGOF' (Buy One Get One Free). This information will help us identify high-value products that are currently being heavily discounted, which can be useful for evaluating our pricing and promotion strategies.
+1.Provide a list of products with a base price greater than 500 and that are featured in promo type of 'BOGOF' (Buy One Get One Free). This information will help us identify high-value products that are currently being heavily discounted, which can be useful for evaluating our pricing and promotion strategies.
 
 Query:
 -------------
@@ -21,7 +21,7 @@ product_name
 Atliq_Double_Bedsheet_set
 Atliq_waterproof_Immersion_Rod	
 
-2.	Generate a report that provides an overview of the number of stores in each city. The results will be sorted in descending order of store counts, allowing us to identify the cities with the highest store presence.The report includes two essential fields: city and store count, which will assist in optimizing our retail operations.
+2.Generate a report that provides an overview of the number of stores in each city. The results will be sorted in descending order of store counts, allowing us to identify the cities with the highest store presence.The report includes two essential fields: city and store count, which will assist in optimizing our retail operations.
 
 Query:
 -------------
@@ -47,7 +47,7 @@ Mangalore	3
 Trivandrum	2
 Vijayawada	2	
 
-3.	Generate a report that displays each campaign along with the total revenue generated before and after the campaign? The report includes three key fields: campaign_name, totaI_revenue(before_promotion), totaI_revenue(after_promotion). This report should help in evaluating the financial impact of our promotional campaigns. (Display the values in millions)
+3.Generate a report that displays each campaign along with the total revenue generated before and after the campaign? The report includes three key fields: campaign_name, totaI_revenue(before_promotion), totaI_revenue(after_promotion). This report should help in evaluating the financial impact of our promotional campaigns. (Display the values in millions)
 
 Query:
 -------------
@@ -70,7 +70,7 @@ Diwali	                82573759	                     207456209
 Sankranti	        58127429	                     140403941
 
 
-4.	Produce a report that calculates the Incremental Sold Quantity (ISU%) for each category during the Diwali campaign. Additionally, provide rankings for the categories based on their ISU%. The report will include three key fields: category, isu%, and rank order. This information will assist in assessing the category-wise success and impact of the Diwali campaign on incremental sales.
+4.Produce a report that calculates the Incremental Sold Quantity (ISU%) for each category during the Diwali campaign. Additionally, provide rankings for the categories based on their ISU%. The report will include three key fields: category, isu%, and rank order. This information will assist in assessing the category-wise success and impact of the Diwali campaign on incremental sales.
 
 Note: ISU% (Incremental Sold Quantity Percentage) is calculated as the percentage increase/decrease in qu	) compared to quantity sold (before promo)
 
@@ -104,7 +104,7 @@ Home Care	   44.33        3
 Personal Care	   23.70        4
 Grocery & Staples  15.29	5
  
-5.	Create a report featuring the Top 5 products, ranked by Incremental Revenue Percentage (IR%), across all campaigns. The report will provide essential information including product name, category, and ir%. This analysis helps identify the most successful products in terms of incremental revenue across our campaigns, assisting in product optimization.
+5.Create a report featuring the Top 5 products, ranked by Incremental Revenue Percentage (IR%), across all campaigns. The report will provide essential information including product name, category, and ir%. This analysis helps identify the most successful products in terms of incremental revenue across our campaigns, assisting in product optimization.
 
 Query:
 -------------

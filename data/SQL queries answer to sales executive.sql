b6c063089fc626e19f4d800577789e9f535435ba
Ajay <83897372+ajaybhupathiraju@users.noreply.github.com>
2024-07-15 23:31:59 +0530

    Update SQL queries answer to sales executive.sql

--- data/SQL queries answer to sales executive.sql
+++ data/SQL queries answer to sales executive.sql
@@ -1,4 +1,4 @@
-									/** Below is a list of questions asked by the business team (senior sales executive). **/
+		/** Below is a list of questions asked by the business team (senior sales executive). **/
 
 
 1.	Provide a list of products with a base price greater than 500 and that are featured in promo type of 'BOGOF' (Buy One Get One Free). This information will help us identify high-value products that are currently being heavily discounted, which can be useful for evaluating our pricing and promotion strategies.
